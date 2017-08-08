f1d9d708a8635566aa47a81fcbb226c1c957f936
luojingzhong <jiufong@126.com>
2017-08-08 10:18:26 +0800

    initialize

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'http://rubygems.org'
+
+
+gem 'rails', '4.0.0'
+gem 'sqlite3', '1.3.8'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails', '2.2.1'
+gem 'turbolinks', '1.1.1'
+gem 'jbuilder', '~> 1.2'
+gem 'rake', '10.4.2'
+gem 'bootstrap-sass', '2.3.2.0'
+group :doc do
+  gem 'sdoc', require: false
+end
+
+group :development, :test do
+  gem 'byebug', platform: :mri
+  gem 'rspec-rails', '2.13.1'
+  gem 'factory_girl_rails','4.2.1'
+  gem 'faker', '1.1.2'
+end
+gem 'bcrypt-ruby', '~> 3.0.0'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Noteserver::Application.config.secret_key_base = '2c7471cd9df0cfad81f6d077818ebe53e064e40af2bbc3c7c2ad4897fb12baca22b890d7b0fbe511ed128f81e6b7bcc93c3f462184ebd4d40869bfa33aa7771b'
